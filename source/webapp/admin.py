888fe2c24039b33510138cf2df3d4520cedb6cba
Chinara <nishanova73@mail.ru>
2022-02-07 13:24:15 +0600

    registered order and order_good models in admin panel

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from webapp.models import Good, Category, Basket
+from webapp.models import Good, Category, Basket, Order, OrderGood
 
 class GoodAdmin(admin.ModelAdmin):
     list_display = ['id', 'description', 'detailed_description', 'category', 'remainder', 'price']
@@ -10,6 +10,22 @@ class GoodAdmin(admin.ModelAdmin):
     fields = ['description', 'detailed_description', 'category', 'remainder', 'price']
     readonly_fields = []
 
+
+class OrderGoodAdmin(admin.TabularInline):
+    model = OrderGood
+    fields = ('good', 'remainder')
+    extra = 0
+
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'phone', 'created_at')
+    list_display_links = ('pk', 'name')
+    ordering = ('-created_at',)
+    inlines = (OrdergoodAdmin,)
+
+
 admin.site.register(Good, GoodAdmin)
 admin.site.register(Category)
 admin.site.register(Basket)
+admin.site.register(Order, OrderAdmin)
